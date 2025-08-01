f1705b4e6ba0b5e1b01d2005d6b27108e9504766
AlexandraFrolova <frolovaalexandra2007@gmail.com>
2025-08-01 19:53:21 +0300

    Add files via upload

---
+++ lab08/file_mul.cpp
@@ -0,0 +1,8 @@
+#include "file_mul.h"
+#include <iostream>
+
+int getMul(int x, int y)
+{
+    std::cout << "getMul(" << x << ", " << y << ") = ";
+    return x*y;
+}
---
+++ lab08/file_sum.cpp
@@ -0,0 +1,6 @@
+#include "file_sum.h"
+
+int getSum(int x, int y)
+{
+    return x+y;
+}
